826a3fbeee4a490c996c517e47fc85caccf1ceec
samwedge <samwedge@gmail.com>
2016-11-28 18:48:30 +0000

    create basic project

---
+++ directory_walker.py
@@ -0,0 +1,11 @@
+"""
+Walks recursively through a given directory path and performs
+the functions in func_list. Each function takes a path as an
+argument and returns a string to write to file
+
+"""
+import os
+
+
+if __name__ == '__main__':
+    path = '/home/sam/test/'
