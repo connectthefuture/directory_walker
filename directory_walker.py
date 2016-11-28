c7fcb35f37e61a34e0189fa6fb67279e9cc47e7d
samwedge <samwedge@gmail.com>
2016-11-28 21:06:46 +0000

    add newline and spaced-dash formatting functions

--- directory_walker.py
+++ directory_walker.py
@@ -15,6 +15,14 @@ def num_files_printer(dir, subdirs, files):
     return str(len(files))
 
 
+def format_newline(dir, subdirs, files):
+    return '\n'
+
+
+def format_spaced_dash(dir, subdirs, files):
+    return ' - '
+
+
 def dir_walker(path, func_list):
     for directory, dirnames, filenames in os.walk(path):
         for func in func_list:
@@ -26,7 +34,9 @@ if __name__ == '__main__':
     def main():
         func_list = [
             dir_printer,
-            num_files_printer
+            format_spaced_dash,
+            num_files_printer,
+            format_newline
         ]
         path = '/home/sam/test/'
         dir_walker(path, func_list)
